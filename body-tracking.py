e58399a989ce1666afdad64c7f471e85f2063013
bizcochillo <abelardop@gmail.com>
2022-09-01 08:51:26 +0200

    Clean pose tracking and add youtube watch

--- body-tracking.py
+++ body-tracking.py
@@ -2,32 +2,16 @@ import cv2
 import mediapipe as mp
 import time
 
-def drawLines (img, points):
-    if len(points) <= 1:
-        return
-    prev = points[0]
-    for i in range(1,len(points)-1):
-        cv2.line(img, prev, points[i], (255, 0, 255), 2)
-        prev=points[i]
-
-
-# First cammera
+# Get camera
 cap = cv2.VideoCapture(1)
 
-mpHands = mp.solutions.pose
-hands = mpHands.Pose()
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
 # Calculate FPS
 pTime = 0
 cTime = 0
-
-# Set up line drawing
-cx_prev      = -1
-cy_prev      = -1
-points       = []
-MAX_LEN      = 150
-INDEX_FINGER = 8
     
 # Analysis loop
 while True:
@@ -35,10 +19,10 @@ while True:
     # Flip image 
     img = cv2.flip(imgRaw, 1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)        
+    results = pose.process(imgRGB)
             
     # Draw lines
-    mpDraw.draw_landmarks(img, results.pose_landmarks, mpHands.POSE_CONNECTIONS)
+    mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
     
     # Draw FPS
     cTime = time.time()
